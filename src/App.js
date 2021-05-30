5672f61d9a68b7e221522a693d8ead18c66995f1
Mahdiyeh <82269782+mahdiye-soleymani@users.noreply.github.com>
2021-05-30 18:48:51 +0430

    rename some files and folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import matchdate from './Components/matchdate'
 class App extends React.Component {
   render() {
     return (
---
+++ src/Components/matchdate/matchdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const matchdate = () => {
+ 
+    return (
+      
+        <h1>matchdate</h1>
+    
+    )
+  
+}
+
+export default matchdate;
